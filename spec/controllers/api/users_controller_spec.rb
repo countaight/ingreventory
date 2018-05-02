c96bf91e744ef4c3144d3194b6738ae11a1b8d40
Oscar Delgadillo <oscar.delgadillo@gmail.com>
2018-05-02 11:12:27 -0500

    Add testing regarding index and showing of users

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController, type: :controller do
 
+	before(:each) do
+		request.accept = 'application/json' if defined? request
+	end
+
+	describe "GET index" do
+
+		it "assigns @users" do
+			user = create(:user)
+			get :index
+			expect(assigns(:users)).to eq([user])
+		end
+
+		it "returns JSON" do
+			user = create(:user)
+			get :index
+			expect(response.content_type).to eq("application/json")
+		end
+
+	end
+
+	describe "GET show" do
+
+		it "assigns @user" do
+			user = create(:user)
+			get :show, params: { id: user.id }
+			expect(assigns(:user)).to eq(user)
+		end
+
+		it "returns JSON" do
+			user = create(:user)
+			get :show, params: { id: user.id }
+			expect(response.content_type).to eq("application/json")
+		end
+
+	end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,22 @@ RSpec.describe User, type: :model do
 		expect(build(:user).save).to be true
 	end
 
-	it "is invalid without a name" do
+	it "is invalid without a username" do
 		expect(build(:user, username: nil).save).to be false
 	end
 
 	it "is invalid without unique username" do
 		user = create(:user)
 		expect(build(:user, username: "genericUsername").save).to be false
+	end
+
+	it "is invalid without an email" do
+		expect(build(:user, email: nil).save).to be false
+	end
+
+	it "is invalid without a unique email address" do
+		user = create(:user)
+		expect(build(:user, email: "user@name.com").save).to be false
 	end
 
 end
